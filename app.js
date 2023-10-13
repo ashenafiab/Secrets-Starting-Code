1b3704746804e54749877628ca4948bd56cb62a8
Ashenafi Abebaw <ashenafi.abebaw@dventus.com>
2023-10-13 16:34:39 +0300

    Database encryption using mongoose-encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -20,6 +21,9 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+const secret = "This_is_my_mongoose_encryption_trial"; 
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
