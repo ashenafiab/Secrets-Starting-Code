8942f3cf7f8777686b65d2daa839ff7151d4a1eb
Ashenafi Abebaw <ashenafi.abebaw@dventus.com>
2023-10-19 14:57:07 +0300

    Level 6 - OAuth 2.0 & How to Implement Sign In with Google

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const session = require('express-session');
-const passport = require('passport');
+const session = require("express-session");
+const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 const app = express();
@@ -34,22 +36,58 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 // CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+
+// Google OAuth 2.0
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ["profile"] }));
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    });
 
 app.get("/register", function (req, res) {
     res.render("register");
@@ -99,12 +137,12 @@ app.post("/login", function (req, res) {
     });
 });
 
-app.get("/logout", function(req, res, next){
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      res.redirect("/");
+app.get("/logout", function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect("/");
     });
-  });
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
